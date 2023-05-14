4a4faa9b121690b5681725b6212a46296dbe942b
nicholas-28 <nika199828@mail.ru>
2023-05-14 16:49:21 +0400

    editing program cs

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -4,32 +4,6 @@ using System.Text;
 
 public class Journal
 {
-    public string JournalFile = "";
-
-
-    public void CreateJournalFile()
-    {
-        if (!File.Exists(JournalFile))
-        {
-            File.CreateText(JournalFile); 
-        }
-    }
-
-    public void DisplayJournalContents()
-    {
-        string journalText = File.ReadAllText(JournalFile);
-        Console.WriteLine(journalText);
-    }
-
-    public void ClearFile()
-    {
-        File.WriteAllText(JournalFile, "");
-    }
-
-    public void AddEntry()
-    {
-        string newLine = Console.ReadLine();
-        File.AppendAllText(JournalFile, newLine); 
-    }
+   
     
 }
