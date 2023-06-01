db8f0e1227f18d3029415c8467ff8f8fbd6dece8
nicholas-28 <nika199828@mail.ru>
2023-06-01 15:22:12 +0400

    program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,5 +1,5 @@
 using System;
-
+using System.Collections.Generic;
 
 class Program
 {
@@ -10,5 +10,25 @@ class Program
         // word.Hide();
         // System.Console.WriteLine(word.GetRenderedText());
         // System.Console.WriteLine(word.IsHidden());
+
+        Reference reference = new Reference("John", 3, 16);
+        string scriptureText = "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.";
+
+        Scripture scripture = new Scripture(reference, scriptureText);
+        Console.WriteLine("Scripture Reference: " + reference.GetFormattedReference());
+
+        while (!scripture.IsCompletelyHidden())
+        {
+            Console.WriteLine("\nPress Enter to hide a random word or type 'quit' to exit:");
+            string input = Console.ReadLine();
+            
+            if(input == "quit")
+            {
+                break;
+            }
+            scripture.HideRandomWord();
+            scripture.ClearConsoleAndDisplay();
+        }
+        Console.WriteLine("\nAll words have been hidden. Program ended.");
     }
 }
