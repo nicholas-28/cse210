bf5ad4e062de64dc964bcbabae384995b765783b
nicholas-28 <nika199828@mail.ru>
2023-06-01 15:22:26 +0400

    scripture update

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -5,6 +5,26 @@ public class Scripture
     private Reference _reference;
     private List<Word> _words;
 
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = CreateWordList(text);
+    }
+
+    private List<Word> CreateWordList(string text)
+    {
+        string[] words = text.Split(' ');
+        List<Word> wordList = new List<Word>();
+
+        foreach (string word in words)
+        {
+            wordList.Add(new Word(word));
+        } 
+
+        return wordList;
+    }
+    
+
     public void HideRandomWord()
     {
         Random random = new Random();
@@ -12,6 +32,20 @@ public class Scripture
         _words[index].Hide();
     }
 
+    public void ClearConsoleAndDisplay()
+    {
+        Console.Clear();
+    }
+
+    private string GetScriptureText()
+    {
+        string scriptureText = _reference.GetFormattedReference() + " ";
+        foreach (Word word in _words)
+        {
+            scriptureText += word.GetRenderedText() + " ";
+        }
+        return scriptureText.Trim();
+    }
     public bool IsCompletelyHidden()
     {
         foreach (Word word in _words)
