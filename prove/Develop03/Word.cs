b536e2f4172693026c7b937b946e8e2200a2534a
nicholas-28 <nika199828@mail.ru>
2023-06-05 16:46:23 +0400

    comments

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+// Represents a single word in the scripture
 public class Word 
 {
     private string _word;
@@ -9,21 +10,23 @@ public class Word
         _word = word;
         _hidden = false;
     }
+    // Hides the word
     public void Hide()
     {
         _hidden = true;
     }
 
-
+    // Shows the word
     public void Show()
     {
         _hidden = false;
     }
-
+    // Checks if the word is hidden
     public bool IsHidden()
     {
         return _hidden;
     }
+    // Returns the rendered text of the word (either the word itself or underscore characters if hidden)
     public string GetRenderedText()
     {
         if (_hidden)
