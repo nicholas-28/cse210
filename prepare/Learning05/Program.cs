a82fe52ae710899fb426179c836958e3c01c27f3
nicholas-28 <nika199828@mail.ru>
2023-07-03 19:45:22 +0400

    Learning 5 do

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square sq = new Square("Green", 4);
+        shapes.Add(sq);
+
+        Rectangle rc = new Rectangle("Blue", 3, 7);
+        shapes.Add(rc);
+
+        Circle cr = new Circle("White", 4);
+        shapes.Add(cr);
+
+        foreach (Shape sh in shapes)
+        {
+            string color = sh.GetColor();
+
+            double area = sh.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+
+
     }
 }
